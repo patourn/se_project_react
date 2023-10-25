9773854ab5fc64aa4fc90ae231f3884c56227e6d
Pablo Tourn <patourn@Pablos-Air.cable.rcn.com>
2023-10-25 06:07:11 -0400

    open and close modal

--- src/App/App.js
+++ src/App/App.js
@@ -8,17 +8,54 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../footer/Footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useState } from "react";
 
 // import "./src/index.js";
 
 function App() {
   const weatherTemp = "102°F";
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleCreateModal = () => {
+    setActiveModal("create");
+  };
+
+  const handleCloseModal = () => {
+    setActiveModal("");
+  };
+
   return (
     <div>
-      <Header />
+      <Header onCreateModal={handleCreateModal} />
       <Main weatherTemp={weatherTemp} />
       <Footer />
-      <ModalWithForm title="New Garment">These are the children</ModalWithForm>
+      {activeModal === "create" && (
+        <ModalWithForm title="New Garment" onClose={handleCloseModal}>
+          <label>
+            Name
+            <input type="text" name="name" minLength="1" maxLength="30" />
+          </label>
+          <label>
+            Image
+            <input type="url" name="link" minLength="1" maxLength="30" />
+          </label>
+          <p>Select the weather type:</p>
+          <div>
+            <div>
+              <input type="radio" id="hot" value="hot" />
+              <label>Hot</label>
+            </div>
+            <div>
+              <input type="radio" id="warm" value="warm" />
+              <label>Warm</label>
+            </div>
+            <div>
+              <input type="radio" id="cold" value="cold" />
+              <label>Cold</label>
+            </div>
+          </div>
+        </ModalWithForm>
+      )}
     </div>
   );
 }
--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -12,7 +12,9 @@ const ModalWithForm = ({
   return (
     <div className={`modal modal_type_${name}`}>
       <div className="modal__content">
-        <button type="button" onClick={onClose} />
+        <button type="button" onClick={onClose}>
+          Close
+        </button>
         <h3>{title}</h3>
         <form>{children}</form>
         <button type="submit">{buttonText}</button>
